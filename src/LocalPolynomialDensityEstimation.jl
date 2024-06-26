bdc4fed05b32c6d04266fc50a8a8e313274bcab1
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-06-26 14:24:21 +0200

    The curve generation works v1

--- src/LocalPolynomialDensityEstimation.jl
+++ src/LocalPolynomialDensityEstimation.jl
@@ -5,44 +5,43 @@ import Statistics: mean
 
 bernstein(n, k, t) = binomial(n, k) .* t .^ k .* (1 .- t) .^ (n - k)
 
-function bezier(points; num = 200)
-    N = length(points)
+function bezier(points, num)
+    n = size(points,2)
     t = LinRange(0, 1, num)
     curve = zeros((2, num))
-    for i = 1:N
-        curve += bernstein(N - 1, i - 1, t)' .* points[:, i]
+    for i = 1:n
+        curve .+= bernstein(n - 1, i - 1, t)' .* points[:, i]
     end
     return curve
 end
 
 
 
-struct Segment
+function segment(p1, p2, angle1, angle2, r; numpoints = 100)
 
-    function Segment(p1, p2, angle1, angle2; r = 0.3, numpoints = 100)
-        d = sqrt(sum((p2 .- p1) .^ 2))
-        r = r * d
-        p = zeros((2, 4))
-        p[:, 1] .= p1
-        p[:, 2] .= p1 .+ [r * cos(angle1), r * sin(angle1)]
-        p[:, 3] .= p2 .+ [r * cos(angle2 + pi), r * sin(angle2 + pi)]
-        p[:, 4] .= p2
-        curve = bezier(s.p, s.numpoints)
-
-        new(p, points, curve)
-
-    end
+    d = sqrt(sum((p2 .- p1) .^ 2))
+    r = r * d
+    p = zeros((2, 4))
+    p[:, 1] .= p1
+    p[:, 2] .= p1 .+ [r * cos(angle1), r * sin(angle1)]
+    p[:, 3] .= p2 .+ [r * cos(angle2 + pi), r * sin(angle2 + pi)]
+    p[:, 4] .= p2
+    bezier(p, numpoints)
 
 end
 
-function get_curve(points)
-    segments = Segment[]
-    for i = 1:(length(points)-1)
-        seg = Segment(points[i, :2], points[i+1, :2], points[i, 2], points[i+1, 2])
-        push!(segments, seg)
+function get_curve(points, r)
+    curve = Matrix{Float64}[]
+    npoints = size(points, 2)
+    for i = 1:(npoints-1)
+        p1 = points[1:2,i]
+        p2 = points[1:2, i+1]
+        a1 = points[3, i]
+        a2 = points[3, i+1]
+        seg = segment(p1, p2, a1, a2, r)
+        push!(curve, seg)
     end
-    curve = vcat([s.curve for s in segments])
-    return segments, curve
+    return hcat(curve...)
 end
 
 
@@ -55,7 +54,7 @@ given an array of points *a*, create a curve through those points.
 - `edgy` is a parameter which controls how "edgy" the curve is,
            edgy=0 is smoothest.
 """
-function get_bezier_curve(a, rad, edgy)
+function get_bezier_curve(a, r, edgy)
     p = atan(edgy) / pi + 0.5
     a = ccw_sort(a)
     a = hcat(a, a[:, 1])
@@ -67,11 +66,9 @@ function get_bezier_curve(a, rad, edgy)
     ang2 = circshift(ang, 1)
     ang = p .* ang1 .+ (1 - p) .* ang2 .+ (abs.(ang2 .- ang1) .> pi) .* pi
     push!(ang, ang[1])
-    #a = hcat(a, ang)
-    #s, c = get_curve(a, r = rad)
-    #x, y = c'
-    #return x, y, a
-    return ang
+    a = vcat(a, ang')
+    c = get_curve(a, r)
+    return c[1,:], c[2,:]
 end
 
 function ccw_sort(p)
