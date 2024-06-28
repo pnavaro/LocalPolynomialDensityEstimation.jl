995b48d1198cc0214d99c94150be126b0f2d0edf
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-06-28 13:55:04 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,22 +3,23 @@ using Test
 using Aqua
 
 @testset "LocalPolynomialDensityEstimation.jl" begin
+
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(LocalPolynomialDensityEstimation)
     end
-    # Write your tests here.
-end
 
-@testset "Bezier curve" begin
-
-    rad = 0.2
-    edgy = 0.05
-    n = 7
-    scale = 1
+    @testset "Bezier curve" begin
     
-    @show a = get_random_points(n, scale)
-    @show x,y, _ = get_bezier_curve(a, rad, edgy)
-
-    @test true
+        radius = 0.2
+        edgy = 0.05
+        n = 7
+        scale = 1
+        
+        a = LocalPolynomialDensityEstimation.get_random_points(n, scale)
+        c = LocalPolynomialDensityEstimation.get_bezier_curve(a, radius, edgy)
+    
+        @test true
+    
+    end
 
 end
