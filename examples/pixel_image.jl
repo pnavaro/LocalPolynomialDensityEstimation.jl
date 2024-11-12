16ce081dc8baa531098fd9f663a937a04b22df03
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-11-13 00:06:27 +0100

    Update pixel_image.jl

--- examples/pixel_image.jl
+++ examples/pixel_image.jl
@@ -8,13 +8,14 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.16.4
 #   kernelspec:
-#     display_name: Julia 1.11.0
+#     display_name: Julia 1.11.1
 #     language: julia
 #     name: julia-1.11
 # ---
 
 using LocalPolynomialDensityEstimation
 using Plots
+using Random
 
 mat = rand(30, 40)
 img = PixelImage(mat, (0, 1), (0, 1))
@@ -29,3 +30,5 @@ mat = zeros(40, 60)
 z = PixelImage(mat, (0, 1), (0, 1))
 
 plot(PlanarPointPattern(z, s))
+
+
