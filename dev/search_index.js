da5064012bed7bb7f0a89a1e2286ddaff6cff227
Documenter.jl <documenter@juliadocs.github.io>
2025-11-24 13:16:43 +0000

    build based on 6e33694

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#LocalPolynomialDensityEstimation","page":"Home","title":"LocalPolynomialDensityEstimation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LocalPolynomialDensityEstimation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#LocalPolynomialDensityEstimation","page":"Home","title":"LocalPolynomialDensityEstimation","text":"Documentation for LocalPolynomialDensityEstimation.\n\n","category":"section"}]
 }
